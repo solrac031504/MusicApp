c634fa7a8825b9055b9cac271c034b2a6607c946
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-13 11:49:07 -0500

    Couple more semicolons

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const db = require('./database.js');
 const app = express();
 
 // Port listening
-const PORT = proces.env.PORT || 5000
+const PORT = proces.env.PORT || 5000;
 
 // Enable cors
 app.use(cors({
@@ -20,7 +20,7 @@ app.use(cors({
         ? process.env.FRONTEND_URL
         : `http://localhost:${PORT}`,
     credentials: true
-}))
+}));
 
 // Only parse json
 app.use(express.json());
