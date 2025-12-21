ef81236291c04bd685e3f00ef267a672f37da19f
solrac031504 <carlos.gonzalez031504@gmail.com>
2025-12-21 15:39:48 -0500

    Modularized authentication functions

--- frontend/src/components/AdminRoute.tsx
+++ frontend/src/components/AdminRoute.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { isAuthenticated, isAdminUser } from '../utilities/authentication';
 
 interface AdminRouteProps {
     children: React.ReactNode;
@@ -7,31 +8,6 @@ interface AdminRouteProps {
 
 const AdminRoute: React.FC<AdminRouteProps> = ({ children}) => {
     const location = useLocation();
-
-    // Check if the user is authenticated
-    const isAuthenticated = (): boolean => {
-        // Get auth details from session storage
-        const user = sessionStorage.getItem('user');
-        const loginExpiration = sessionStorage.getItem('loginExpiration');
-
-        const loginExpirationDate = new Date(loginExpiration!);
-
-        const nowUTC = new Date();
-
-        // Return true if user exists AND now is before loginExpiration (in UTC)
-        return ((!!user) && (nowUTC <= loginExpirationDate));
-    }
-
-    // Check if the user is one of the admin users
-    const isAdminUser = (): boolean => {
-        // Get auth details from session storage
-        const isAdmin = sessionStorage.getItem('isAdmin') === 'true' ?
-            true
-            : false;
-
-        // Return true if user exists AND now is before loginExpiration (in UTC) AND user is admin
-        return isAdmin;
-    }
 
     // If the user is not authenticated go to the login
     if (!isAuthenticated()) {
--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { isAuthenticated } from '../utilities/authentication'
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
@@ -7,20 +8,6 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     const location = useLocation();
-    
-    // Check if the user is authenticated
-    const isAuthenticated = (): boolean => {
-        // Get auth details from local storage
-        const user = sessionStorage.getItem('user');
-        const loginExpiration = sessionStorage.getItem('loginExpiration');
-
-        const loginExpirationDate = new Date(loginExpiration!);
-
-        const nowUTC = new Date();
-
-        // Return true if user exists AND now is before loginExpiration (in UTC)
-        return ((!!user) && (nowUTC <= loginExpirationDate));
-    }
 
     if (!isAuthenticated()) {
         // Redirect to login if not authenticated
